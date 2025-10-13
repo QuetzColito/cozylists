e00527b829b82dd11005d841d67799ac22111176
Stefan Lahne <stefan.lahne@proton.me>
2025-10-14 01:27:34 +0200

    add a List Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import type { Component } from "solid-js";
 import { createSignal } from "solid-js";
 import "./styles/style.scss";
 import Egg from "./Egg";
+import List from "./List";
 
 const App: Component = () => {
   return (
     <>
       <Egg />
+      <List />
     </>
   );
 };
--- src/List.tsx
+++ src/List.tsx
@@ -1,36 +1,67 @@
 import type { Component } from "solid-js";
+import { For, Show } from "solid-js";
 import { createSignal } from "solid-js";
 import "./styles/style.scss";
 
-const Egg: Component = () => {
-  const [count, setCount] = createSignal(0);
-  const [right, setRight] = createSignal(30);
-  const [top, setTop] = createSignal(30);
+const List: Component = () => {
+  const [items, setItems] = createSignal([
+    {
+      name: "Mushoku Tensei",
+    },
+    {
+      name: "Isekai Shikkaku",
+    },
+    {
+      name: "Steins;Gate",
+    },
+  ]);
 
-  const handleClick = () => {
-    setCount(count() + 1);
-    setRight(
-      Math.random() *
-        (document.body.offsetWidth -
-          (document.getElementById("egg")?.offsetWidth || 0)),
-    );
-    setTop(
-      Math.random() *
-        (document.body.offsetHeight -
-          (document.getElementById("egg")?.offsetHeight || 0)),
-    );
-  };
+  const [selected, set_selected] = createSignal(0);
+  const [editing, set_editing] = createSignal(-1);
+
+  document.addEventListener("keydown", (e) => {
+    if (editing != null) {
+      if (e.key == "Enter" || e.key == "Escape") {
+        setItems(
+          items().map((item, index) =>
+            index != selected()
+              ? item
+              : {
+                  name: (
+                    document.getElementById(
+                      editing().toString(),
+                    ) as HTMLInputElement
+                  ).value,
+                },
+          ),
+        );
+        set_editing(-1);
+      }
+    }
+    if (e.key == "j") set_selected((selected() + 1) % items().length);
+    if (e.key == "k") set_selected((selected() - 1) % items().length);
+    if (e.key == "i") {
+      set_editing(selected());
+      document.getElementById(selected().toString())?.focus();
+    }
+  });
 
   return (
-    <button
-      onClick={handleClick}
-      class="egg"
-      id="egg"
-      style={{ right: `${right()}px`, top: `${top()}px` }}
-    >
-      {count()}
-    </button>
+    <>
+      <For each={items()}>
+        {(item, index) => (
+          <li class={selected() == index() ? "active" : ""}>
+            <Show when={index() != editing()}>
+              <span> {item.name} </span>
+            </Show>
+            <Show when={index() == editing()}>
+              <input id={index().toString()} value={item.name} />
+            </Show>
+          </li>
+        )}
+      </For>
+    </>
   );
 };
 
-export default Egg;
+export default List;
